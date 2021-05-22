f2a79d00d290755e7d1b0979a35da2dabac9b830
wafelack <wafelack@protonmail.com>
2021-05-22 11:04:46 +0200

    style: Format code.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use nix::unistd::{fork, ForkResult};
-use std::{time::{Duration, Instant}, fs, process::{Command, exit}, io::{stdin, BufRead}, os::unix::fs::MetadataExt};
 use clap::{App, Arg};
+use nix::unistd::{fork, ForkResult};
 use std::thread;
+use std::{
+    fs,
+    io::{stdin, BufRead},
+    os::unix::fs::MetadataExt,
+    process::{exit, Command},
+    time::{Duration, Instant},
+};
 
 macro_rules! gen_app {
     () => {
@@ -54,14 +60,14 @@ macro_rules! error {
 }
 pub type Result<T> = std::result::Result<T, Error>;
 
-fn get_edit_time(file: String) -> Result<i64>{
+fn get_edit_time(file: String) -> Result<i64> {
     match fs::metadata(&file) {
         Ok(m) => Ok(m.mtime()),
         Err(e) => error!("Failed to query file metadata: {}: {}.", file, e),
     }
 }
 
-fn try_main() -> Result<()>{
+fn try_main() -> Result<()> {
     let matches = gen_app!().get_matches();
 
     let mut raw_command = matches.values_of("command").unwrap();
@@ -72,14 +78,15 @@ fn try_main() -> Result<()>{
     let strict = matches.is_present("strict");
     let ttl = match matches.value_of("ttl") {
         Some(v) => match v.parse::<i32>() {
-            Ok(v) => if v < 0 {
-                None
-            } else {
-                Some(v)
+            Ok(v) => {
+                if v < 0 {
+                    None
+                } else {
+                    Some(v)
+                }
             }
             Err(_) => None,
-
-        }
+        },
         None => None,
     };
     let delay = match matches.value_of("delay") {
@@ -87,29 +94,40 @@ fn try_main() -> Result<()>{
         None => 1.0,
     };
 
-    let files = stdin().lock().lines().map(|l| match l {
-        Ok(l) => Ok(l),
-        Err(_) => error!("Failed to read file name from standard input."),
-    }).collect::<Result<Vec<_>>>()?.into_iter().filter(|f| !f.is_empty()).collect::<Vec<_>>();
-    let mut edit_dates = files.iter().map(|f| {
-        get_edit_time(f.to_string())
-    }).collect::<Result<Vec<i64>>>()?;
-    
+    let files = stdin()
+        .lock()
+        .lines()
+        .map(|l| match l {
+            Ok(l) => Ok(l),
+            Err(_) => error!("Failed to read file name from standard input."),
+        })
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .filter(|f| !f.is_empty())
+        .collect::<Vec<_>>();
+    let mut edit_dates = files
+        .iter()
+        .map(|f| get_edit_time(f.to_string()))
+        .collect::<Result<Vec<i64>>>()?;
+
     match fork() {
         Ok(ForkResult::Parent { .. }) => Ok(()),
         Ok(ForkResult::Child) => {
             let start = Instant::now();
-            loop { 
+            loop {
                 match ttl {
-                    Some(ttl) => if start.elapsed().as_secs() > ttl as u64 {
-                        return Ok(());
+                    Some(ttl) => {
+                        if start.elapsed().as_secs() > ttl as u64 {
+                            return Ok(());
+                        }
                     }
                     None => {}
                 }
                 thread::sleep(Duration::from_secs_f32(delay));
-                let new_edits = files.iter().map(|f| {
-                    get_edit_time(f.to_string())
-                }).collect::<Result<Vec<i64>>>()?;
+                let new_edits = files
+                    .iter()
+                    .map(|f| get_edit_time(f.to_string()))
+                    .collect::<Result<Vec<i64>>>()?;
 
                 for (idx, new) in new_edits.iter().enumerate() {
                     let prev = edit_dates[idx];
@@ -119,30 +137,45 @@ fn try_main() -> Result<()>{
                         if verbose {
                             println!("{} rlgl: `{}` has changed.", GREEN_STAR, fname);
                         }
-                        let (status, stdout) = match Command::new(command)
-                            .args(&args)
-                            .output() {
-                                Ok(s) => (s.status, s.stdout),
-                                Err(e) => {
-                                    if verbose {
-                                        eprintln!("{} rlgl: `{}`: {}.", RED_STAR, format!("{} {}", command, args.join(" ")), e);
-                                    }
-                                    if strict {
-                                        return error!("Failed to execute `{}`: {}.", format!("{} {}", command, args.join(" ")), e);
-                                    } else {
-                                        continue;
-                                    }
+                        let (status, stdout) = match Command::new(command).args(&args).output() {
+                            Ok(s) => (s.status, s.stdout),
+                            Err(e) => {
+                                if verbose {
+                                    eprintln!(
+                                        "{} rlgl: `{}`: {}.",
+                                        RED_STAR,
+                                        format!("{} {}", command, args.join(" ")),
+                                        e
+                                    );
                                 }
-                            };
+                                if strict {
+                                    return error!(
+                                        "Failed to execute `{}`: {}.",
+                                        format!("{} {}", command, args.join(" ")),
+                                        e
+                                    );
+                                } else {
+                                    continue;
+                                }
+                            }
+                        };
                         if !quiet {
                             println!("{}", String::from_utf8_lossy(&stdout).trim());
                         }
                         if !status.success() {
                             if verbose {
-                                eprintln!("{} rlgl: `{}` failed, exit code: {}.", RED_STAR, format!("{} {}", command, args.join(" " )), status.code().unwrap_or(1));
+                                eprintln!(
+                                    "{} rlgl: `{}` failed, exit code: {}.",
+                                    RED_STAR,
+                                    format!("{} {}", command, args.join(" ")),
+                                    status.code().unwrap_or(1)
+                                );
                             }
                             if strict {
-                                return error!("`{}` execution failed. Aborting due to `--strict`.", format!("{} {}", command, args.join(" ")));
+                                return error!(
+                                    "`{}` execution failed. Aborting due to `--strict`.",
+                                    format!("{} {}", command, args.join(" "))
+                                );
                             }
                         }
                         break;
